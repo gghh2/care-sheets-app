8b9fd4fcf7319a27187406eda9126f2ce9701026
WarpZone <nioche.nicolas@gmail.com>
2025-09-27 12:37:52 +0000

    Add Netlify serverless functions

---
+++ netlify/functions/drive-proxy.js
@@ -0,0 +1,36 @@
+exports.handler = async (event, context) => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
+    return { statusCode: 200, headers };
+  }
+
+  const { path, token } = JSON.parse(event.body || '{}');
+  
+  try {
+    const response = await fetch(`https://www.googleapis.com/drive/v3/${path}`, {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    });
+    
+    const data = await response.json();
+    
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(data)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
